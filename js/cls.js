14fe01101cf48d0ae462774b7f9984148ae53120
Fahmid Imtiaz <fahmidimtiaz13@gmail.com>
2022-10-03 20:44:32 +0600

    new

--- js/cls.js
+++ js/cls.js
@@ -1,6 +1,37 @@
-console.log("Hello");
+// console.log("Hello");
 
-var friendName = "Fahim";
-console.log(friendName);
-console.log(typeof friendName);
+// var friendName = "Fahim";
+// console.log(friendName);
+// console.log(typeof friendName);
 
+
+var promise = "Lorem ipsum is placeholder text commonly used in the layouts and visual mockups.";
+// console.log(promise);
+// console.log(promise.toLowerCase());
+// console.log(promise.toUpperCase());
+// console.log(promise.indexOf('commonly'));
+// console.log(promise.split('commonly'));
+
+
+// var price1 = 25;
+// var price2 = 15;
+
+// // var total = price1 / price2;
+// var total = price1 % price2;
+// console.log(total);
+
+// var price1 = 5.023145697;
+
+// var result = Math.round(price1);
+// var result = Math.ceil(price1);
+
+// console.log(result);
+
+
+var price = 8;
+
+if(price > 10){
+    console.log('This is true.');
+}else{
+    console.log('This is false.');
+}
---
+++ js/fundamentalCls.js
@@ -0,0 +1,32 @@
+// // console.log("Fundamental Classes")
+// // console.log("Fundamental Classes special")
+
+// var friendsAge = [25,23,22,26,20,21];
+// // console.log(friendsAge);
+// // console.log(friendsAge[3]);
+
+// // var fahimAge = friendsAge[2];
+// // var position = friendsAge.indexOf(20);
+
+// // console.log("Fahim's age is: " + fahimAge);
+// // console.log("Fahim's age position is: " + position);
+// friendsAge.push(24);
+// friendsAge.push(19);
+// console.log(friendsAge);
+// console.log(friendsAge.length);
+// friendsAge.pop()
+// console.log(friendsAge);
+
+
+
+var manName = ['kalam', 'balam', 'salam'];
+console.log(manName);
+
+manName.push('palam')
+console.log(manName);
+
+manName.shift();
+console.log(manName);
+
+manName.unshift('new_name_1', 'new_name_2');
+console.log(manName);
